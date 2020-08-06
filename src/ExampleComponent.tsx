401b7907a547c5c1a278be6378576827d0f90cef
Tom Wey <tom.wey@theguardian.com>
2020-08-06 10:49:38 +0100

    Use src libs and emotion in the ExampleComponent

--- src/ExampleComponent.tsx
+++ src/ExampleComponent.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { css } from "@emotion/core";
+import { palette } from "@guardian/src-foundations";
+import { Button } from "@guardian/src-button";
 
 type Props = {};
 
-export const ExampleComponent: React.FC<Props> = () => <div>Hello</div>;
+const wrapperStyles = css`
+  background-color: ${palette.neutral[97]};
+`;
+
+export const ExampleComponent: React.FC<Props> = () => (
+  <div css={wrapperStyles}>
+    <span>Hello from a Braze Banner!</span>
+    <Button>Click me!</Button>
+  </div>
+);
