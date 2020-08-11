ffc71bd26b6ace3484f9ec2902893a4740b4c827
Tom Wey <tom.wey@theguardian.com>
2020-08-11 09:57:14 +0100

    This configuration works
    I feel like I shouldn’t need the window.guardian approach for sharing
    deps though. But leaving this as a waypoint.

--- rollup.config.js
+++ rollup.config.js
@@ -7,8 +7,14 @@ import replace from "@rollup/plugin-replace";
 import alias from "@rollup/plugin-alias";
 import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import externalGlobals from "rollup-plugin-external-globals";
 
 const extensions = [...DEFAULT_EXTENSIONS, ".ts", ".tsx"];
+
+const globals = {
+  react: "guardian.automat.preact",
+  "@emotion/core": "guardian.automat.emotionCore",
+};
 
 const configs = [["./index.ts", "./dist/index.js"]].map(([input, file]) => ({
   input: input,
@@ -17,14 +23,15 @@ const configs = [["./index.ts", "./dist/index.js"]].map(([input, file]) => ({
     format: "cjs",
     sourcemap: false,
   },
+  external: (id) => Object.keys(globals).some((key) => id == key),
   plugins: [
     peerDepsExternal(),
-    alias({
-      entries: [
-        { find: "react", replacement: "preact/compat" },
-        { find: "react-dom", replacement: "preact/compat" },
-      ],
-    }),
+    // alias({
+    //   entries: [
+    //     { find: "react", replacement: "preact-x/compat" },
+    //     { find: "react-dom", replacement: "preact-x/compat" },
+    //   ],
+    // }),
     resolve({ extensions: extensions }),
     commonjs(),
     replace({ "process.env.NODE_ENV": '"production"' }),
@@ -34,6 +41,7 @@ const configs = [["./index.ts", "./dist/index.js"]].map(([input, file]) => ({
     }),
     terser(),
     filesize(),
+    externalGlobals(globals),
   ],
 }));
 
