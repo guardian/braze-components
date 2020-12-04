60b049630f5db29eac201110b9507bb3389af77b
Tom Wey <tom.wey@theguardian.com>
2020-12-04 15:18:28 +0000

    First pass at signing a i.guim.co.uk image

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import awsServerlessExpress from 'aws-serverless-express';
 import { Context } from 'aws-lambda';
+import { format } from '@guardian/image';
+
+const SALT = process.env.IMAGE_SALT;
 
 const app = express();
 app.use(express.json());
@@ -8,6 +11,19 @@ app.use(express.json());
 app.get('/healthcheck', (req: express.Request, res: express.Response) => {
     res.header('Content-Type', 'text/plain');
     res.send('OK');
+});
+
+app.post('/signImage', (req: express.Request, res: express.Response) => {
+    const { imageUrl } = req.body;
+
+    const profile = {
+        width: 600,
+        quality: 45,
+    };
+
+    const signedUrl = format(imageUrl, SALT, profile);
+
+    res.send({ signedUrl });
 });
 
 const PORT = process.env.PORT || 3030;
