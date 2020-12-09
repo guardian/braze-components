528b068ffc94c8dde173d84718aecb3ccef753ad
Rhys Mills <rhystmills@gmail.com>
2020-12-09 10:47:00 +0000

    Replaced NODE_ENV environment query with hostname-based solution supporting dev, code and prod Also surfaced isCode and isProduction methods in advance of login PR

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,7 +1,12 @@
-const isDevelopment = (): boolean => process.env.NODE_ENV === 'development';
+const matchesHostname = (hostname: string): boolean => window.location.hostname === hostname;
 
-const GRID_URL = isDevelopment()
-    ? 'https://media.test.dev-gutools.co.uk'
-    : 'https://media.gutools.co.uk';
+const isDevelopment = (): boolean => matchesHostname('braze.storybook.local.dev-gutools.co.uk');
+const isCode = (): boolean => matchesHostname('braze-components.code.dev-gutools.co.uk ');
+const isProduction = (): boolean => matchesHostname('braze-components.gutools.co.uk');
 
-export { isDevelopment, GRID_URL };
+const GRID_URL =
+    isDevelopment() || isCode()
+        ? 'https://media.test.dev-gutools.co.uk'
+        : 'https://media.gutools.co.uk';
+
+export { isDevelopment, isCode, isProduction, GRID_URL };
