5583c38920794d612f909b988d0d471b3e1e2f34
Rhys Mills <rhystmills@gmail.com>
2020-12-09 14:12:56 +0000

    Split env querying into two files - one for storybook env, and one for component env Grid addon uses storybook env

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,12 +1,3 @@
-const matchesHostname = (hostname: string): boolean => window.location.hostname === hostname;
+const isDevelopment = (): boolean => process.env.NODE_ENV === 'development';
 
-const isDevelopment = (): boolean => matchesHostname('braze.storybook.local.dev-gutools.co.uk');
-const isCode = (): boolean => matchesHostname('braze-components.code.dev-gutools.co.uk ');
-const isProduction = (): boolean => matchesHostname('braze-components.gutools.co.uk');
-
-const GRID_URL =
-    isDevelopment() || isCode()
-        ? 'https://media.test.dev-gutools.co.uk'
-        : 'https://media.gutools.co.uk';
-
-export { isDevelopment, isCode, isProduction, GRID_URL };
+export { isDevelopment };
