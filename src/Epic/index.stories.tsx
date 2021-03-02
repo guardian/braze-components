6992af976fcdf930e09c0dbef1f6f09f3b696101
Rhys Mills <rhystmills@gmail.com>
2021-03-02 11:42:10 +0000

    Moved all consts to top of index.stories.tsx

--- src/Epic/index.stories.tsx
+++ src/Epic/index.stories.tsx
@@ -24,6 +24,20 @@ const epicWrapper = css`
     max-width: 620px;
     margin: 10px;
 `;
+const TOTAL_PARAGRAPHS = 9;
+const defaultContent = {
+    heading: 'Since you’re here...',
+    paragraphs: [
+        '... we have a small favour to ask. More people, like you, are reading and supporting the Guardian’s independent, investigative journalism than ever before. And unlike many news organisations, we made the choice to keep our reporting open for all, regardless of where they live or what they can afford to pay.',
+        'The Guardian will engage with the most critical issues of our time – from the escalating climate catastrophe to widespread inequality to the influence of big tech on our lives. At a time when factual information is a necessity, we believe that each of us, around the world, deserves access to accurate reporting with integrity at its heart.',
+        'Our editorial independence means we set our own agenda and voice our own opinions. Guardian journalism is free from commercial and political bias and not influenced by billionaire owners or shareholders. This means we can give a voice to those less heard, explore where others turn away, and rigorously challenge those in power.',
+        'We hope you will consider supporting us today. We need your support to keep delivering quality journalism that’s open and independent. Every reader contribution, however big or small, is so valuable. ',
+    ],
+    highlightedText:
+        'Support The Guardian from as little as %%CURRENCY_SYMBOL%%1 - and it only takes a minute. Thank you.',
+    buttonText: 'Support The Guardian',
+    buttonUrl: 'https://support.theguardian.com/contribute',
+};
 
 declare global {
     interface Window {
@@ -85,22 +99,6 @@ const Epic: React.FC<EpicProps> = (props) => {
     }
 
     return null;
-};
-
-const TOTAL_PARAGRAPHS = 9;
-
-const defaultContent = {
-    heading: 'Since you’re here...',
-    paragraphs: [
-        '... we have a small favour to ask. More people, like you, are reading and supporting the Guardian’s independent, investigative journalism than ever before. And unlike many news organisations, we made the choice to keep our reporting open for all, regardless of where they live or what they can afford to pay.',
-        'The Guardian will engage with the most critical issues of our time – from the escalating climate catastrophe to widespread inequality to the influence of big tech on our lives. At a time when factual information is a necessity, we believe that each of us, around the world, deserves access to accurate reporting with integrity at its heart.',
-        'Our editorial independence means we set our own agenda and voice our own opinions. Guardian journalism is free from commercial and political bias and not influenced by billionaire owners or shareholders. This means we can give a voice to those less heard, explore where others turn away, and rigorously challenge those in power.',
-        'We hope you will consider supporting us today. We need your support to keep delivering quality journalism that’s open and independent. Every reader contribution, however big or small, is so valuable. ',
-    ],
-    highlightedText:
-        'Support The Guardian from as little as %%CURRENCY_SYMBOL%%1 - and it only takes a minute. Thank you.',
-    buttonText: 'Support The Guardian',
-    buttonUrl: 'https://support.theguardian.com/contribute',
 };
 
 const buildProps = (data: DataFromKnobs): EpicProps => {
