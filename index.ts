9f666589647c07272913c0e7cb96095ac68382f1
Michael Clapham <michaeljclapham@gmail.com>
2021-05-11 16:47:58 +0100

    Use pkgu to compile typescript

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
 export * from './src/BrazeMessageComponent';
+export * from './src/logic/BrazeMessages';
+export * from './src/logic/NullBrazeMessages';
+export * from './src/logic/LocalMessageCache';
