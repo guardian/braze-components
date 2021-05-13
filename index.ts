9670ad65f427830f4ba0fb63b90b1c81b83aa665
Michael Clapham <michaeljclapham@gmail.com>
2021-05-13 10:11:58 +0100

    Switch back to rollup

--- index.ts
+++ index.ts
@@ -1,4 +1,1 @@
 export * from './src/BrazeMessageComponent';
-export * from './src/logic/BrazeMessages';
-export * from './src/logic/NullBrazeMessages';
-export * from './src/logic/LocalMessageCache';
