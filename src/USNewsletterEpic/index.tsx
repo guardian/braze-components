3d756c81143bdf3e3fce0caf0e880ad79b207181
Tom Wey <tom.wey@theguardian.com>
2021-07-02 17:17:59 +0100

    Update clock icon with version from src-icons
    Not actually available yet, but I’ve opened a PR to add a new SvgClock
    icon based on the markup in the source figma:
    https://github.com/guardian/source/pull/843

--- src/USNewsletterEpic/index.tsx
+++ src/USNewsletterEpic/index.tsx
@@ -1,5 +1,5 @@
 import { css, ThemeProvider } from '@emotion/react';
-import React from 'react';
+import React, { ReactElement } from 'react';
 import { brand } from '@guardian/src-foundations';
 import { Button, buttonBrandAlt } from '@guardian/src-button';
 import { styles as commonStyles } from '../styles/bannerCommon';
@@ -8,6 +8,22 @@ import { body, textSans } from '@guardian/src-foundations/typography';
 import { canRender } from './canRender';
 import { OphanComponentEvent } from '@guardian/types';
 import { BrazeClickHandler } from '../utils/tracking';
+
+// Once https://github.com/guardian/source/pull/843 is merged and in a
+// @guardian/src-icons release we'll be able to bump the version on this project
+// and replace this with:
+// import { SvgClock } from '@guardian/src-icons'
+export const SvgClock = (): ReactElement => {
+    return (
+        <svg viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M26 15c0 6.075-4.925 11-11 11S4 21.075 4 15 8.925 4 15 4s11 4.925 11 11zm-9.8-.35L15.475 6H14.5l-.75 9.375 1.275 1.275L22 16v-1l-5.8-.35z"
+            />
+        </svg>
+    );
+};
 
 const IMAGE_URL =
     'https://i.guim.co.uk/img/media/d0944e021b1cc7426f515fecc8034f12b7862041/0_0_784_784/784.png?width=196&s=fbdead3f454e1ceeeab260ffde71100a';
@@ -37,6 +53,12 @@ const styles = {
         position: relative;
         top: 2px;
         margin-right: 4px;
+
+        svg {
+            fill: #999999;
+            height: 16px;
+            width: 16px;
+        }
     `,
     frequencyText: css`
         color: #333333;
@@ -86,7 +108,9 @@ export const USNewsletterEpic: React.FC<Props> = (props: Props) => {
                 <div css={styles.rightSection}>
                     <span css={commonStyles.heading}>{header}</span>
                     <div css={styles.frequencySection}>
-                        <span css={styles.frequencyClock}>{clockSVG}</span>
+                        <span css={styles.frequencyClock}>
+                            <SvgClock />
+                        </span>
                         <span css={styles.frequencyText}>{frequency}</span>
                     </div>
                     <p css={commonStyles.paragraph}>{paragraph1}</p>
@@ -99,26 +123,5 @@ export const USNewsletterEpic: React.FC<Props> = (props: Props) => {
         </ThemeProvider>
     );
 };
-
-const clockSVG = (
-    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect width="16" height="16" fill="#E5E5E5" />
-        <g clipPath="url(#clip0)">
-            <rect width="1440" height="2342" transform="translate(-510 -1706)" fill="white" />
-            <rect width="620" height="283" transform="translate(-180 -46)" fill="#F6F6F6" />
-            <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M7.71183 0.208008C11.9866 0.208008 15.4666 3.7096 15.4666 8.01094C15.4666 12.3123 11.9866 15.7923 7.71183 15.7923C3.43702 15.7923 0 12.3123 0 8.01094C0 3.7096 3.43702 0.208008 7.71183 0.208008ZM7.19628 1.63458L6.65924 8.27033L7.73331 9.17815L13.1896 8.72424V7.66511L8.72146 7.40573L8.24886 1.63458H7.19628Z"
-                fill="#999999"
-            />
-        </g>
-        <defs>
-            <clipPath id="clip0">
-                <rect width="1440" height="2342" fill="white" transform="translate(-510 -1706)" />
-            </clipPath>
-        </defs>
-    </svg>
-);
 
 export { COMPONENT_NAME };
