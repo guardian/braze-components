5a9a76d5aa0d7430eef7ecc0d7289c08f5d12c14
Michael Clapham <michaeljclapham@gmail.com>
2021-07-05 20:12:49 +0100

    Use head request to check local login service

--- .storybook/gu-auth/register.js
+++ .storybook/gu-auth/register.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { addons, types } from '@storybook/addons';
 
 import { GuAuth } from './GuAuth';
+import { skipAuth } from '../utils';
 
 const GU_AUTH_ADDON_ID = 'guAuthAddon';
 const GU_AUTH_PANEL_ID = `${GU_AUTH_ADDON_ID}/panel`;
 
-if (process.env.STORYBOOK_DISABLE_AUTH !== 'true') {
-    addons.register(GU_AUTH_ADDON_ID, () => {
-        addons.add(GU_AUTH_PANEL_ID, {
-            title: 'Gu Auth',
-            type: types.TOOL,
-            render: () => <GuAuth />,
-            paramKey: 'guAuth', // This is the key under which parameters are expected in the addon config
+skipAuth().then((skip) => {
+    if (!skip && process.env.STORYBOOK_DISABLE_AUTH !== 'true') {
+        addons.register(GU_AUTH_ADDON_ID, () => {
+            addons.add(GU_AUTH_PANEL_ID, {
+                title: 'Gu Auth',
+                type: types.TOOL,
+                render: () => <GuAuth />,
+                paramKey: 'guAuth', // This is the key under which parameters are expected in the addon config
+            });
         });
-    });
-}
+    } else {
+        console.log("Skipping auth requirement");
+    }
+});
