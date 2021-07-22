b527d9db82122e2966bcba9a4afc8bb5290562f0
Michael Clapham <michaeljclapham@gmail.com>
2021-07-22 13:32:08 +0100

    Add tsconfig to External epic folder to fix css prop error in VS code

--- index.ts
+++ index.ts
@@ -1,5 +1,1 @@
 export * from './src/BrazeMessageComponent';
-
-// Temporary to fix css prop error in Epic story
-import * as epicStories from './src/ExternalEpic/index.stories';
-console.log(epicStories);
