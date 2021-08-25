3aa362f163e6bcf16bd89bec422f9454f896d6d5
Michael Clapham <michaeljclapham@gmail.com>
2021-08-25 20:35:39 +0100

    Add checkbox for simulating subscribe failure

--- src/AUNewsletterEpic/index.stories.tsx
+++ src/AUNewsletterEpic/index.stories.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from 'react';
 import { BrazeEndOfArticleComponent } from '../BrazeEndOfArticleComponent';
 import { StorybookWrapper } from '../utils/StorybookWrapper';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 
 export default {
     component: 'AUNewsletterEpic',
@@ -25,6 +25,7 @@ export const defaultStory = (): ReactElement | null => {
     );
     const componentName = text('componentName', 'AUNewsletterEpic');
     const ophanComponentId = text('ophanComponentId', 'example_ophan_component_id');
+    const simulateFailure = boolean('Simulate Subscribe Failure', false);
 
     return (
         <StorybookWrapper>
@@ -39,7 +40,9 @@ export const defaultStory = (): ReactElement | null => {
                 }}
                 subscribeToNewsletter={(newsletterId) => {
                     console.log(`subscribeToNewsletter invoked with id ${newsletterId}`);
-                    return new Promise((resolve) => setTimeout(resolve, 1000));
+                    return new Promise((resolve, reject) =>
+                        setTimeout(() => (simulateFailure ? reject() : resolve()), 1000),
+                    );
                 }}
             />
         </StorybookWrapper>
--- src/UKNewsletterEpic/index.stories.tsx
+++ src/UKNewsletterEpic/index.stories.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from 'react';
 import { BrazeEndOfArticleComponent } from '../BrazeEndOfArticleComponent';
 import { StorybookWrapper } from '../utils/StorybookWrapper';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 
 export default {
     component: 'UKNewsletterEpic',
@@ -25,6 +25,7 @@ export const defaultStory = (): ReactElement | null => {
     );
     const componentName = text('componentName', 'UKNewsletterEpic');
     const ophanComponentId = text('ophanComponentId', 'example_ophan_component_id');
+    const simulateFailure = boolean('Simulate Subscribe Failure', false);
 
     return (
         <StorybookWrapper>
@@ -40,7 +41,7 @@ export const defaultStory = (): ReactElement | null => {
                 subscribeToNewsletter={(newsletterId) => {
                     console.log(`subscribeToNewsletter invoked with id ${newsletterId}`);
                     return new Promise((resolve, reject) =>
-                        setTimeout(() => (Math.random() > 0.5 ? resolve() : reject()), 1000),
+                        setTimeout(() => (simulateFailure ? reject() : resolve()), 1000),
                     );
                 }}
             />
