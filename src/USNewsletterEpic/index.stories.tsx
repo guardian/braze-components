caac52773410954218720afbb3a4507a7420e257
Tom Wey <tom.wey@theguardian.com>
2021-09-09 10:08:34 +0100

    Migrate USNewsletterEpic from knobs to controls

--- src/USNewsletterEpic/index.stories.tsx
+++ src/USNewsletterEpic/index.stories.tsx
@@ -1,50 +1,72 @@
 import React, { ReactElement } from 'react';
 import { BrazeEndOfArticleComponent } from '../BrazeEndOfArticleComponent';
 import { StorybookWrapper } from '../utils/StorybookWrapper';
-import { withKnobs, text } from '@storybook/addon-knobs';
 import { knobsData } from '../utils/knobsData';
 
 export default {
     component: 'USNewsletterEpic',
     title: 'EndOfArticle/USNewsletterEpic',
-    decorators: [
-        withKnobs({
-            escapeHTML: false,
-        }),
-    ],
     parameters: {},
+    argTypes: {
+        slotName: {
+            name: 'slotName',
+            type: { name: 'string', required: true },
+            description: 'The slot on the page',
+            table: {
+                category: "core (don't change)",
+            },
+        },
+        componentName: {
+            name: 'componentName',
+            type: { name: 'string', required: true },
+            description: 'The component (template) name',
+            table: {
+                category: "core (don't change)",
+            },
+        },
+        header: {
+            name: 'header',
+            type: { name: 'string', required: true },
+            description: 'Header text',
+        },
+        frequency: {
+            name: 'frequency',
+            type: { name: 'string', required: true },
+            description: 'Text description of how often the email is sent',
+        },
+        paragraph1: {
+            name: 'paragraph1',
+            type: { name: 'string', required: true },
+            description: 'First paragraph',
+        },
+        paragraph2: {
+            name: 'paragraph2',
+            type: { name: 'string', required: false },
+            description: 'Second paragraph',
+        },
+        ophanComponentId: {
+            name: 'ophanComponentId',
+            type: { name: 'string', required: true },
+            description: 'The component ID sent to Ophan for tracking',
+        },
+    },
 };
 
-export const defaultStory = (): ReactElement | null => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const slotName = text('slotName', 'EndOfArticle');
-    const header = text('header', 'First Thing');
-    const frequency = text('frequency', 'Daily');
-    const paragraph1 = text(
-        'paragraph1',
-        'Start your day with a global perspective on America. Get the Guardian’s top stories and must reads in one hit – every weekday.',
-    );
-    const paragraph2 = text(
-        'paragraph2',
-        'We thought you should know this newsletter may contain information about Guardian products and services.',
-    );
-    const componentName = text('componentName', 'USNewsletterEpic');
-    const ophanComponentId = text('ophanComponentId', 'example_ophan_component_id');
-
+const StoryTemplate = (args): ReactElement | null => {
     const brazeMessageProps = {
-        header,
-        frequency,
-        paragraph1,
-        paragraph2,
-        ophanComponentId,
+        header: args.header,
+        frequency: args.frequency,
+        paragraph1: args.paragraph1,
+        paragraph2: args.paragraph2,
+        ophanComponentId: args.ophanComponentId,
     };
 
-    knobsData.set({ ...brazeMessageProps, componentName });
+    knobsData.set({ ...brazeMessageProps, componentName: args.componentName });
 
     return (
         <StorybookWrapper>
             <BrazeEndOfArticleComponent
-                componentName={componentName}
+                componentName={args.componentName}
                 brazeMessageProps={brazeMessageProps}
                 subscribeToNewsletter={(newsletterId) => {
                     console.log(`subscribeToNewsletter invoked with id ${newsletterId}`);
@@ -55,4 +77,18 @@ export const defaultStory = (): ReactElement | null => {
     );
 };
 
-defaultStory.storyName = 'USNewsletterEpic';
+export const DefaultStory = StoryTemplate.bind({});
+
+DefaultStory.args = {
+    slotName: 'EndOfArticle',
+    header: 'First Thing',
+    frequency: 'Daily',
+    paragraph1:
+        'Start your day with a global perspective on America. Get the Guardian’s top stories and must reads in one hit – every weekday.',
+    paragraph2:
+        'We thought you should know this newsletter may contain information about Guardian products and services.',
+    componentName: 'USNewsletterEpic',
+    ophanComponentId: 'example_ophan_component_id',
+};
+
+DefaultStory.storyName = 'USNewsletterEpic';
