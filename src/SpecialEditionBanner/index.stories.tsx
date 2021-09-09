f10971b21dc63877acacdcdfdf21c267c8e83394
Tom Wey <tom.wey@theguardian.com>
2021-09-09 16:13:21 +0100

    Migrate SpecialEditionBanner from knobs to controls

--- src/SpecialEditionBanner/index.stories.tsx
+++ src/SpecialEditionBanner/index.stories.tsx
@@ -1,52 +1,62 @@
 import React, { ReactElement } from 'react';
-import { withKnobs, text } from '@storybook/addon-knobs';
 import { BrazeBannerComponent } from '../BrazeBannerComponent';
 import { StorybookWrapper } from '../utils/StorybookWrapper';
 import { knobsData } from '../utils/knobsData';
+import { coreArgTypes } from '../storybookCommon/argTypes';
+import { BrazeMessageProps } from '.';
 
 export default {
     component: 'SpecialEditionBanner',
     title: 'Banner/SpecialEditionBanner',
-    decorators: [withKnobs],
-    parameters: {
-        knobs: {
-            escapeHTML: false, // Block HTML escaping, preventing double-escaping of imgUrl special characters in Storybook
+    argTypes: {
+        ...coreArgTypes,
+        header: {
+            name: 'header',
+            type: { name: 'string', required: true },
+            description: 'Header text',
+        },
+        body: {
+            name: 'body',
+            type: { name: 'string', required: true },
+            description: 'Body text',
         },
     },
 };
 
-export const defaultStory = (): ReactElement => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const slotName = text('slotName', 'Banner');
-    const header = text('header', "A year you'd rather forget?");
-    const body = text(
-        'body',
-        "2020 has been a shocker of a year, but there were some bright spots shining through the gloom: untold heroism, invention, kindness, scientific breakthroughs, communal spirit and moments of great independent journalism. We've gathered it all for you in our latest special edition: The best of a bad year.",
-    );
-    const componentName = text('componentName', 'SpecialEditionBanner');
+const StoryTemplate = (args: BrazeMessageProps & { componentName: string }): ReactElement => {
+    const brazeMessageProps = {
+        header: args.header,
+        body: args.body,
+    };
 
     // This is to make the data available to the guPreview add-on:
-    knobsData.set({ header, body, componentName });
+    knobsData.set({ ...brazeMessageProps, componentName: args.componentName });
 
     return (
         <StorybookWrapper>
             <>
                 <BrazeBannerComponent
-                    componentName={componentName}
+                    componentName={args.componentName}
                     logButtonClickWithBraze={(internalButtonId) => {
                         console.log(`Button with internal ID ${internalButtonId} clicked`);
                     }}
                     submitComponentEvent={(componentEvent) => {
                         console.log('submitComponentEvent called with: ', componentEvent);
                     }}
-                    brazeMessageProps={{
-                        header: header,
-                        body: body,
-                    }}
+                    brazeMessageProps={brazeMessageProps}
                 />
             </>
         </StorybookWrapper>
     );
 };
 
-defaultStory.story = { name: 'SpecialEditionBanner' };
+export const DefaultStory = StoryTemplate.bind({});
+
+DefaultStory.args = {
+    slotName: 'Banner',
+    header: "A year you'd rather forget?",
+    body: "2020 has been a shocker of a year, but there were some bright spots shining through the gloom: untold heroism, invention, kindness, scientific breakthroughs, communal spirit and moments of great independent journalism. We've gathered it all for you in our latest special edition: The best of a bad year.",
+    componentName: 'SpecialEditionBanner',
+};
+
+DefaultStory.story = { name: 'SpecialEditionBanner' };
