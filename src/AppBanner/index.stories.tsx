2321370f5e85a98e023909727cfae39a82166585
Tom Wey <tom.wey@theguardian.com>
2021-09-09 15:40:03 +0100

    Migrate AppBanner from knobs to controls
    Need to test how this affects the image picker.

--- src/AppBanner/index.stories.tsx
+++ src/AppBanner/index.stories.tsx
@@ -1,60 +1,79 @@
 import React, { ReactElement } from 'react';
-import { withKnobs, text } from '@storybook/addon-knobs';
 import { BrazeBannerComponent } from '../BrazeBannerComponent';
 import { StorybookWrapper } from '../utils/StorybookWrapper';
 import { knobsData } from '../utils/knobsData';
 import { withGrid, grid } from '../../.storybook/grid/withGrid';
+import { BrazeMessageProps } from '.';
+import { coreArgTypes } from '../storybookCommon/argTypes';
 
 export default {
     component: 'AppBanner',
     title: 'Banner/AppBanner',
-    decorators: [withGrid, withKnobs],
+    decorators: [withGrid],
     parameters: {
-        knobs: {
-            escapeHTML: false, // Block HTML escaping, preventing double-escaping of imgUrl special characters in Storybook
-        },
         grid: {
             disable: false,
         },
     },
+    argTypes: {
+        ...coreArgTypes,
+        header: {
+            name: 'header',
+            type: { name: 'string', required: true },
+            description: 'Header text',
+        },
+        body: {
+            name: 'body',
+            type: { name: 'string', required: true },
+            description: 'Body text',
+        },
+        cta: {
+            name: 'cta',
+            type: { name: 'string', required: true },
+            description: 'Text next to the app store logos',
+        },
+    },
 };
 
-export const defaultStory = (): ReactElement => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const slotName = text('slotName', 'Banner');
-    const header = text('header', 'A note to our digital subscribers');
-    const body = text(
-        'body',
-        'Hi John, did you know that as a Guardian digital subscriber you can enjoy an enhanced experience of our quality, independent journalism on all your devices, including The Guardian Live app.',
-    );
-    const componentName = text('componentName', 'AppBanner');
+const StoryTemplate = (args: BrazeMessageProps & { componentName: string }): ReactElement => {
     const imageUrl = grid(
         'https://i.guim.co.uk/img/media/de6813b4dd9b9805a2d14dd6af14ae2b48e2e19e/0_0_930_520/master/930.png?quality=45&width=930&s=0beb53509265d32e3d201aa3981323bb',
     );
-    const cta = text('cta', 'Search for "Guardian live news"');
+
+    const brazeMessageProps = {
+        header: args.header,
+        body: args.body,
+        cta: args.cta,
+        imageUrl,
+    };
 
     // This is to make the data available to the guPreview add-on:
-    knobsData.set({ header, body, componentName, imageUrl, cta });
+    knobsData.set({ ...brazeMessageProps, componentName: args.componentName });
 
     return (
         <StorybookWrapper>
             <BrazeBannerComponent
-                componentName={componentName}
+                componentName={args.componentName}
                 logButtonClickWithBraze={(internalButtonId) => {
                     console.log(`Button with internal ID ${internalButtonId} clicked`);
                 }}
                 submitComponentEvent={(componentEvent) => {
                     console.log('submitComponentEvent called with: ', componentEvent);
                 }}
-                brazeMessageProps={{
-                    header,
-                    body,
-                    imageUrl,
-                    cta,
-                }}
+                brazeMessageProps={brazeMessageProps}
             />
         </StorybookWrapper>
     );
 };
 
-defaultStory.story = { name: 'AppBanner' };
+export const DefaultStory = StoryTemplate.bind({});
+
+DefaultStory.args = {
+    slotName: 'Banner',
+    header: 'A note to our digital subscribers',
+    body: 'Hi John, did you know that as a Guardian digital subscriber you can enjoy an enhanced experience of our quality, independent journalism on all your devices, including The Guardian Live app.',
+    componentName: 'AppBanner',
+    cta: 'Search for "Guardian live news"',
+};
+
+DefaultStory.story = { name: 'AppBanner' };
