761affbd8ff62ae10f41bc1e7691ef6ba62d2554
Tom Wey <tom.wey@theguardian.com>
2021-09-09 10:41:51 +0100

    Migrate Epic from knobs to controls

--- src/Epic/index.stories.tsx
+++ src/Epic/index.stories.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement } from 'react';
-import { withKnobs, text } from '@storybook/addon-knobs';
 import { StorybookWrapper } from '../utils/StorybookWrapper';
 import { BrazeEndOfArticleComponent } from '../BrazeEndOfArticleComponent';
 import { knobsData } from '../utils/knobsData';
@@ -7,27 +6,96 @@ import { knobsData } from '../utils/knobsData';
 export default {
     component: 'Epic',
     title: 'EndOfArticle/Epic',
-    decorators: [withKnobs],
-    parameters: {
-        knobs: {
-            escapeHTML: false,
+    parameters: {},
+    argTypes: {
+        slotName: {
+            name: 'slotName',
+            type: { name: 'string', required: true },
+            description: 'The slot on the page',
+            table: {
+                category: "core (don't change)",
+            },
+        },
+        componentName: {
+            name: 'componentName',
+            type: { name: 'string', required: true },
+            description: 'The component (template) name',
+            table: {
+                category: "core (don't change)",
+            },
+        },
+        heading: {
+            name: 'heading',
+            type: { name: 'string', required: true },
+            description: 'Header text',
+        },
+        paragraph1: {
+            name: 'paragraph1',
+            type: { name: 'string', required: true },
+            description: 'First paragraph',
+        },
+        paragraph2: {
+            name: 'paragraph2',
+            type: { name: 'string' },
+            description: 'Second paragraph',
+        },
+        paragraph3: {
+            name: 'paragraph3',
+            type: { name: 'string' },
+            description: 'Third paragraph',
+        },
+        paragraph4: {
+            name: 'paragraph4',
+            type: { name: 'string' },
+            description: 'Fourth paragraph',
+        },
+        paragraph5: {
+            name: 'paragraph5',
+            type: { name: 'string' },
+            description: 'Fifth paragraph',
+        },
+        paragraph6: {
+            name: 'paragraph6',
+            type: { name: 'string' },
+            description: 'Sixth paragraph',
+        },
+        paragraph7: {
+            name: 'paragraph7',
+            type: { name: 'string' },
+            description: 'Seventh paragraph',
+        },
+        paragraph8: {
+            name: 'paragraph8',
+            type: { name: 'string' },
+            description: 'Eighth paragraph',
+        },
+        paragraph9: {
+            name: 'paragraph9',
+            type: { name: 'string' },
+            description: 'Ninth paragraph',
+        },
+        highlightedText: {
+            name: 'highlightedText',
+            type: { name: 'string' },
+            description: 'Yellow highlighted text at end of final paragraph. Supports HTML.',
+        },
+        buttonText: {
+            name: 'buttonText',
+            type: { name: 'string', required: true },
+            description: 'Button label text',
+        },
+        buttonUrl: {
+            name: 'buttonUrl',
+            type: { name: 'string', required: true },
+            description:
+                'Destination link for the button. See Braze user guide for tracking params.',
+        },
+        ophanComponentId: {
+            name: 'ophanComponentId',
+            type: { name: 'string', required: true },
+            description: 'The component ID sent to Ophan for tracking',
         },
     },
-};
-
-const TOTAL_PARAGRAPHS = 9;
-const defaultContent = {
-    heading: 'Since you’re here...',
-    paragraphs: [
-        '... we have a small favour to ask. More people, <a href="https://example.com">like you</a>, are reading and supporting the Guardian’s independent, investigative journalism than ever before. And unlike many news organisations, we made the choice to keep our reporting open for all, regardless of where they live or what they can afford to pay.',
-        'The Guardian will engage with the most critical issues of our time – from the escalating climate catastrophe to widespread inequality to the influence of big tech on our lives. At a time when factual information is a necessity, we believe that each of us, around the world, deserves access to accurate reporting with integrity at its heart.',
-        'Our editorial independence means we set our own agenda and voice our own opinions. Guardian journalism is free from commercial and political bias and not influenced by billionaire owners or shareholders. This means we can give a voice to those less heard, explore where others turn away, and rigorously challenge those in power.',
-        'We hope you will consider supporting us today. We need your support to keep delivering quality journalism that’s open and independent. Every reader contribution, however big or small, is so valuable. ',
-    ],
-    highlightedText:
-        'Support <a href="https://example.com">The Guardian</a> from as little as %%CURRENCY_SYMBOL%%1 - and it only takes a minute. Thank you.',
-    buttonText: 'Support The Guardian',
-    buttonUrl: 'https://support.theguardian.com/contribute',
 };
 
 declare global {
@@ -36,48 +104,57 @@ declare global {
     }
 }
 
-export const defaultStory = (): ReactElement | null => {
-    const slotName = text('slotName', 'EndOfArticle');
-    const componentName = text('componentName', 'Epic');
-    const ophanComponentId = text('ophanComponentId', 'example_ophan_component_id');
-    const heading = text('heading', defaultContent.heading);
-    const highlightedText = text('highlightedText', defaultContent.highlightedText);
-    const buttonText = text('buttonText', defaultContent.buttonText);
-    const buttonUrl = text('buttonUrl', defaultContent.buttonUrl);
-    const paragraphMap: { [key: string]: string } = {};
-
-    // Add existing paragraphs as knobs, and then pad out with empty knobs
-    for (let i = 0; i < TOTAL_PARAGRAPHS; i++) {
-        const name = `paragraph${i + 1}`;
-        const currentParagraph = defaultContent.paragraphs[i];
-        if (currentParagraph) {
-            paragraphMap[name] = text(name, currentParagraph);
-        } else {
-            paragraphMap[name] = text(name, '');
-        }
-    }
-
+const StoryTemplate = (args): ReactElement | null => {
     const brazeMessageProps = {
-        slotName,
-        ophanComponentId,
-        heading,
-        highlightedText,
-        buttonText,
-        buttonUrl,
-        ...paragraphMap,
+        slotName: args.slotName,
+        ophanComponentId: args.ophanComponentId,
+        heading: args.heading,
+        highlightedText: args.highlightedText,
+        buttonText: args.buttonText,
+        buttonUrl: args.buttonUrl,
+        paragraph1: args.paragraph1,
+        paragraph2: args.paragraph2,
+        paragraph3: args.paragraph3,
+        paragraph4: args.paragraph4,
+        paragraph5: args.paragraph5,
+        paragraph6: args.paragraph6,
+        paragraph7: args.paragraph7,
+        paragraph8: args.paragraph8,
+        paragraph9: args.paragraph9,
     };
 
-    knobsData.set({ ...brazeMessageProps, componentName });
+    knobsData.set({ ...brazeMessageProps, componentName: args.componentName });
 
     return (
         <StorybookWrapper>
             <BrazeEndOfArticleComponent
                 brazeMessageProps={brazeMessageProps}
-                componentName={componentName}
+                componentName={args.componentName}
                 subscribeToNewsletter={() => Promise.resolve()}
             />
         </StorybookWrapper>
     );
 };
 
-defaultStory.story = { name: 'Epic' };
+export const DefaultStory = StoryTemplate.bind({});
+
+DefaultStory.args = {
+    heading: 'Since you’re here...',
+    paragraph1:
+        '... we have a small favour to ask. More people, <a href="https://example.com">like you</a>, are reading and supporting the Guardian’s independent, investigative journalism than ever before. And unlike many news organisations, we made the choice to keep our reporting open for all, regardless of where they live or what they can afford to pay.',
+    paragraph2:
+        'The Guardian will engage with the most critical issues of our time – from the escalating climate catastrophe to widespread inequality to the influence of big tech on our lives. At a time when factual information is a necessity, we believe that each of us, around the world, deserves access to accurate reporting with integrity at its heart.',
+    paragraph3:
+        'Our editorial independence means we set our own agenda and voice our own opinions. Guardian journalism is free from commercial and political bias and not influenced by billionaire owners or shareholders. This means we can give a voice to those less heard, explore where others turn away, and rigorously challenge those in power.',
+    paragraph4:
+        'We hope you will consider supporting us today. We need your support to keep delivering quality journalism that’s open and independent. Every reader contribution, however big or small, is so valuable. ',
+    highlightedText:
+        'Support <a href="https://example.com">The Guardian</a> from as little as %%CURRENCY_SYMBOL%%1 - and it only takes a minute. Thank you.',
+    buttonText: 'Support The Guardian',
+    buttonUrl: 'https://support.theguardian.com/contribute',
+    ophanComponentId: 'example_ophan_component_id',
+    slotName: 'EndOfArticle',
+    componentName: 'Epic',
+};
+
+DefaultStory.story = { name: 'Epic' };
