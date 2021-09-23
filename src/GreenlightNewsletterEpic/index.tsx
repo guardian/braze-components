2779d81188515d177fbfe7abb3a8fdd62da9d240
Michael Clapham <michaeljclapham@gmail.com>
2021-09-23 15:32:03 +0100

    Correct newsletter id for Greenlight

--- src/GreenlightNewsletterEpic/index.tsx
+++ src/GreenlightNewsletterEpic/index.tsx
@@ -6,7 +6,7 @@ const IMAGE_URL =
     'https://i.guim.co.uk/img/media/4b8aadcdc15499e3730ce5372f305f3b30dc6858/0_0_2520_2520/2520.png?width=196&s=f1768efe97cfff31c0636b77304f4655';
 //'https://i.guim.co.uk/img/media/699af09e8a6e0f921429229db0df343769254b72/0_0_1400_1400/1400.png?width=196&s=af685980d319c87fc8efcccae65c7a51';
 
-const newsletterId = '4148';
+const newsletterId = '4147';
 
 export type BrazeMessageProps = {
     header?: string;
