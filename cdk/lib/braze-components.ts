86be7dee17615cb7c258fdf82bab1eafadba9c73
Tom Wey <tom.wey@theguardian.com>
2021-12-21 15:58:26 +0000

    Add missing / prefix

--- cdk/lib/braze-components.ts
+++ cdk/lib/braze-components.ts
@@ -52,7 +52,7 @@ export class BrazeComponents extends GuStack {
                     s3OriginSource: {
                         s3BucketSource: sourceBucket,
                         originAccessIdentity,
-                        originPath: `${this.stage}/braze-components-storybook-static`,
+                        originPath: `/${this.stage}/braze-components-storybook-static`,
                     },
                     behaviors: [{ isDefaultBehavior: true }],
                 },
