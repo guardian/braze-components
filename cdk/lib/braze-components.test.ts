45a7acf9b44bf9d68539354e49ee627774e880c2
Tom Wey <tom.wey@theguardian.com>
2022-10-06 12:54:29 +0100

    Upgrade @guardian/cdk to v48.0.0
    Quite a bit has changed since we introduced @guardian/cdk so there are
    some changes to our CDK implementation. I think the diffs in the
    snapshot make sense.

--- cdk/lib/braze-components.test.ts
+++ cdk/lib/braze-components.test.ts
@@ -1,12 +1,17 @@
-import '@aws-cdk/assert/jest';
-import { SynthUtils } from '@aws-cdk/assert';
-import { App } from '@aws-cdk/core';
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
 import { BrazeComponents } from './braze-components';
 
 describe('The BrazeComponents stack', () => {
     it('matches the snapshot', () => {
         const app = new App();
-        const stack = new BrazeComponents(app, 'BrazeComponents', { stack: 'targeting' });
-        expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+        const stack = new BrazeComponents(app, 'BrazeComponents', {
+            stack: 'targeting',
+            stage: 'TEST',
+            domainName: 'braze-components.test.dev-gutools.co.uk',
+            tlsCertId: 'xxx-xxx-xxx',
+        });
+        const template = Template.fromStack(stack);
+        expect(template.toJSON()).toMatchSnapshot();
     });
 });
--- cdk/lib/braze-components.ts
+++ cdk/lib/braze-components.ts
@@ -1,16 +1,23 @@
-import { Certificate } from '@aws-cdk/aws-certificatemanager';
-import { OriginAccessIdentity, ViewerCertificate } from '@aws-cdk/aws-cloudfront';
-import { Bucket } from '@aws-cdk/aws-s3';
-import type { App } from '@aws-cdk/core';
-import { Duration } from '@aws-cdk/core';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import { GuCname } from '@guardian/cdk/lib/constructs/dns';
-import { GuCloudFrontWebDistribution } from './GuCloudFrontWebDistribution';
+import type { App } from 'aws-cdk-lib';
+import { Duration } from 'aws-cdk-lib';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+import {
+    CloudFrontWebDistribution,
+    OriginAccessIdentity,
+    ViewerCertificate,
+} from 'aws-cdk-lib/aws-cloudfront';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 
+export interface BrazeComponentsStackProps extends GuStackProps {
+    tlsCertId: string;
+    domainName: string;
+}
 export class BrazeComponents extends GuStack {
-    constructor(scope: App, id: string, props: GuStackProps) {
-        super(scope, id, { ...props, migratedFromCloudFormation: true });
+    constructor(scope: App, id: string, props: BrazeComponentsStackProps) {
+        super(scope, id, props);
 
         const bucketNameFromStaticCloudformationStack = 'braze-components-storybook';
         const sourceBucket = Bucket.fromBucketName(
@@ -20,68 +27,42 @@ export class BrazeComponents extends GuStack {
         );
 
         const originAccessIdFromStaticCloudformationStack = 'E3EA9DC41190PP';
-        const originAccessIdentity = OriginAccessIdentity.fromOriginAccessIdentityName(
+        const originAccessIdentity = OriginAccessIdentity.fromOriginAccessIdentityId(
             this,
             'braze-components-origin-access-identity',
             originAccessIdFromStaticCloudformationStack,
         );
 
-        const tlsCertId = this.withStageDependentValue({
-            app: 'braze-components',
-            variableName: 'certificateId',
-            stageValues: {
-                PROD: 'eca82256-dff4-4b0a-80d6-7f884a1ee92d',
-                CODE: '1df4da51-49e5-4dd4-b136-3c5e1cac9d64',
-            },
-        });
-
-        const domainNameMappings = {
-            CODE: 'braze-components.code.dev-gutools.co.uk',
-            PROD: 'braze-components.gutools.co.uk',
-        };
-
-        const customDomainName = this.withStageDependentValue({
-            app: 'braze-components',
-            variableName: 'customDomainName',
-            stageValues: domainNameMappings,
-        });
-
         const certificate = Certificate.fromCertificateArn(
             this,
             'braze-components-cert',
-            `arn:aws:acm:us-east-1:${this.account}:certificate/${tlsCertId}`,
+            `arn:aws:acm:us-east-1:${this.account}:certificate/${props.tlsCertId}`,
         );
 
-        const cloudFrontDist = new GuCloudFrontWebDistribution(
-            this,
-            'braze-components-cloudfront',
-            {
-                originConfigs: [
-                    {
-                        s3OriginSource: {
-                            s3BucketSource: sourceBucket,
-                            originAccessIdentity,
-                            originPath: `/${this.stage}/braze-components-storybook-static`,
-                        },
-                        behaviors: [{ isDefaultBehavior: true }],
+        const cloudFrontDist = new CloudFrontWebDistribution(this, 'braze-components-cloudfront', {
+            originConfigs: [
+                {
+                    s3OriginSource: {
+                        s3BucketSource: sourceBucket,
+                        originAccessIdentity,
+                        originPath: `/${this.stage}/braze-components-storybook-static`,
                     },
-                ],
-                viewerCertificate: ViewerCertificate.fromAcmCertificate(certificate, {
-                    aliases: [customDomainName],
-                }),
-                existingLogicalId: {
-                    logicalId: 'CDN',
-                    reason: 'We are adopting the existing CloudFront dist so that we can migrate to CDK without downtime.',
+                    behaviors: [{ isDefaultBehavior: true }],
                 },
-            },
-        );
+            ],
+            viewerCertificate: ViewerCertificate.fromAcmCertificate(certificate, {
+                aliases: [props.domainName],
+            }),
+        });
+
+        this.overrideLogicalId(cloudFrontDist, {
+            logicalId: 'CDN',
+            reason: 'We are adopting the existing CloudFront dist so that we can migrate to CDK without downtime.',
+        });
 
         new GuCname(this, 'DNS entry', {
             app: 'braze-components',
-            domainNameProps: {
-                CODE: { domainName: 'braze-components.code.dev-gutools.co.uk' },
-                PROD: { domainName: 'braze-components.gutools.co.uk' },
-            },
+            domainName: props.domainName,
             resourceRecord: cloudFrontDist.distributionDomainName,
             ttl: Duration.hours(1),
         });
