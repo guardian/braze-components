0b8c3cfab812b270a7a5531b067f7359fcdef18d
Rik Roots <rik.roots@gmail.com>
2022-11-14 10:00:45 +0000

    refactor code following PR comments

--- src/Epic/ContributionsEpicButtons.tsx
+++ src/Epic/ContributionsEpicButtons.tsx
@@ -127,7 +127,7 @@ export const ContributionsEpicButtons = ({
     hidePaymentIcons,
 }: ContributionsEpicButtonsProps): JSX.Element => {
     const [sectionState, setSectionState] = useState<SectionState>('DEFAULT');
-    const showPaymentIcons = hidePaymentIcons != null && hidePaymentIcons === 'true' ? false : true;
+    const showPaymentIcons = hidePaymentIcons !== 'true';
 
     if (sectionState === 'REMINDER_CONFIRMED') {
         return (
