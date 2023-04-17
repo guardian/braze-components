7d115aa0577dc26761f4ae634f932dd1d61f7c27
Rik Roots <rik.roots@gmail.com>
2023-04-17 13:04:05 +0100

    added additional canRender checks for the authored epic

--- src/EpicWithSpecialHeader/canRender.ts
+++ src/EpicWithSpecialHeader/canRender.ts
@@ -1,3 +1,18 @@
-export { canRender } from '../Epic/canRender';
+import { BrazeMessageProps } from '../Epic/index';
+import { canRender as epicCanRender } from '../Epic/canRender';
 
 export const COMPONENT_NAME = 'EpicWithSpecialHeader';
+
+export const canRender = (brazeMessageProps: BrazeMessageProps): boolean => {
+	let canRenderResult = epicCanRender(brazeMessageProps);
+	if (canRenderResult) {
+    	const { authoredEpicImageUrl, authoredEpicImageAltText, authoredEpicBylineName } = brazeMessageProps;
+
+    	return Boolean(
+	        authoredEpicImageUrl &&
+	            authoredEpicImageAltText &&
+	            authoredEpicBylineName,
+	    );
+	}
+	return canRenderResult;
+};
