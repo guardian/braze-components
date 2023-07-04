63132c2e2bf1f7586b5801959754c284c4c3d50c
Tom Forbes <tom.forbes@theguardian.com>
2023-07-04 13:49:01 +0100

    fix build + tests

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,9 @@ module.exports = {
     roots: ['<rootDir>/src'],
     modulePathIgnorePatterns: ['<rootDir>/src/factories/test.ts'],
     transformIgnorePatterns: ['/node_modules/(?!(@guardian)/)'],
-    globals: {
-        'ts-jest': {
+    transform: {
+        '^.+\\.tsx?$': ['ts-jest', {
             tsconfig: 'tsconfig.test.json',
-        },
+        }]
     },
 };
