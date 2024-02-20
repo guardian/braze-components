e2f4c5179d2acdba6436711360fc0c4ac5c54fd8
Rik Roots <rik.roots@gmail.com>
2024-02-20 12:39:38 +0000

    Fix discrepency between BannerWithLink and StyleableBannerWithLink attributes

--- src/BannerWithLink/index.tsx
+++ src/BannerWithLink/index.tsx
@@ -10,6 +10,7 @@ export type BrazeMessageProps = {
     header?: string;
     body?: string;
     boldText?: string;
+    highlight?: string;
     buttonText?: string;
     buttonUrl?: string;
     imageUrl?: string;
@@ -23,6 +24,13 @@ export type Props = {
 export const BannerWithLink: React.FC<Props> = (props: Props) => {
     if (!canRender(props.brazeMessageProps)) {
         return null;
+    }
+
+    const { boldText } = props.brazeMessageProps;
+
+    // StyleableBannerWithLink uses `highlight` instead of `boldText` attribute - this check fixes the discrepency so Marketing won't have to change existing canvases
+    if (boldText) {
+        props.brazeMessageProps.highlight = boldText;
     }
 
     return <StyleableBannerWithLink {...props}></StyleableBannerWithLink>;
